4b674fc6e8854ae0d14861b69051c9ee2ae72448
Daniel Ramirez <dramlin1010@g.educaand.es>
2024-10-16 18:46:34 +0200

    Ciertas correciones hechas | Threading Implementado | Posibilidad de Decisiones

--- server.py
+++ server.py
@@ -8,8 +8,6 @@ import os
 config = configparser.ConfigParser()
 config.read('aws.conf')
 
-aws_access_key = config.get('config', 'access_key')
-aws_secret_key = config.get('config', 'aws_secret_access_key')
 aws_region = config.get('config', 'aws_region')
 class Tftp():
     def __init__(self, ip, puerto, directorio):
@@ -22,20 +20,14 @@ class Tftp():
     
     def escuchar(self):
         self.server.listen(self.ip, self.puerto)
-"""
-def check_bucket(bucket_name):
-    tf = Terraform(working_dir='terraform/')
-    direccion = "aws_s3_bucket.bucket_ejemplo"
-    args = ['import', direccion, bucket_name]
 
-    respuesta = tf.cmd(*args)
+    def iniciar_server(self):
+         thread = threading.Thread(target=self.escuchar())
+         thread.daemon = True
+         thread.start()    
 
-    if respuesta == 0:
-        print(f"[+] El bucket {bucket_name} ya existe.")
-    else:
-        print(f"[-] El bucket {bucket_name} no existe, creandolo.....")
-        crear_bucket(bucket_name)
-"""
+# APLICAR MEJORA DE PODER CREAR VARIOS BUCKETS A LA VEZ, PARA ELLO HAY QUE VERIFICAR SI EL ARCHIVO EXISTE. EN ESE CASO NO INTRODUCIR PROVIDER
+# Y TENER QUE ENUMERAR CADA nombre de bucket resource.
 
 def crear_bucket(bucket_name):
         tf_config = f"""
@@ -51,17 +43,26 @@ resource "aws_s3_bucket" "bucket_ejemplo" {{
         """
         directorio_terraform = 'terraform/'
         
-        with open(os.path.join(directorio_terraform, 'initial.tf'), 'w') as f:
+        with open(os.path.join(directorio_terraform, 'bucket.tf'), 'w') as f: # PARA NUEVA ACTUALIZACION PARA IMPLEMENTAR VARIOS BUCKETS DEBEREMOS DE CAMBIARLO a 'a' APPEND.
             f.write(tf_config)
 
         tf = Terraform(working_dir=directorio_terraform)
         tf.init(capture_output=False)
         tf.apply(skip_plan=True, capture_output=False)
 
+        # resultado = (0, None, None) -> Cuando no hay cambios
+
+
 if __name__ == "__main__":
-    #check_bucket("s3-bucket-daniel-test")
-    crear_bucket("s3-bucket-daniel-test")
-    exit(0) # DE PRUEBAS DE MOMENTO
+
+    pregunta = input("Quieres crear un bucket? [S o N]: ")
+
+    if pregunta == "Y" or pregunta == "S" or pregunta == "Si" or pregunta == "si":
+        nombre_bucket = input("[Opcional] Como quieres que se llame el bucket (Ejemplo -> s3-bucket-NOMBRE-test): ")
+        crear_bucket(nombre_bucket)
+        print("\n[+] Inicializando el servidor TFTP\n")
+    elif pregunta == "":
+        exit(0)
     mitfpt = Tftp('0.0.0.0',69,'archivos')
     mitfpt.asignar_directorio()
-    mitfpt.escuchar()
+    mitfpt.iniciar_server()
